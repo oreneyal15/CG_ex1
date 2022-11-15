00f98f93f939d3618e19a44267b51f7745b6b88c
orene <oren.eyal@mail.huji.ac.il>
2022-11-15 15:38:45 +0200

    init ex1

---
+++ EX1/Assets/CharacterAnimator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public TextAsset BVHFile; // The BVH file that defines the animation and skeleton
+    public bool animate; // Indicates whether or not the animation should be running
+    public bool interpolate; // Indicates whether or not frames should be interpolated
+    [Range(0.01f, 2f)] public float animationSpeed = 1; // Controls the speed of the animation playback
+
+    public BVHData data; // BVH data of the BVHFile will be loaded here
+    public float t = 0; // Value used to interpolate the animation between frames
+    public float[] currFrameData; // BVH channel data corresponding to the current keyframe
+    public float[] nextFrameData; // BVH vhannel data corresponding to the next keyframe
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BVHParser parser = new BVHParser();
+        data = parser.Parse(BVHFile);
+    }
+
+    // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
+    public Matrix4x4 RotateTowardsVector(Vector3 v)
+    {
+        // Your code here
+        return Matrix4x4.zero;
+    }
+
+    // Creates a Cylinder GameObject between two given points in 3D space
+    public GameObject CreateCylinderBetweenPoints(Vector3 p1, Vector3 p2, float diameter)
+    {
+        // Your code here
+        return null;
+    }
+
+    // Creates a GameObject representing a given BVHJoint and recursively creates GameObjects for it's child joints
+    public GameObject CreateJoint(BVHJoint joint, Vector3 parentPosition)
+    {
+        // Your code here
+        return null;
+    }
+
+    // Transforms BVHJoint according to the keyframe channel data, and recursively transforms its children
+    public void TransformJoint(BVHJoint joint, Matrix4x4 parentTransform)
+    {
+        // Your code here
+    }
+
+    // Returns the frame nunmber of the BVH animation at a given time
+    public int GetFrameNumber(float time)
+    {
+        // Your code here
+        return 0;
+    }
+
+    // Returns the proportion of time elapsed between the last frame and the next one, between 0 and 1
+    public float GetFrameIntervalTime(float time)
+    {
+        // Your code here
+        return 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time * animationSpeed;
+        if (animate)
+        {
+            int currFrame = GetFrameNumber(time);
+            // Your code here
+        }
+    }
+}
---
+++ EX1/Assets/QuaternionUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class QuaternionUtils
+{
+    // The default rotation order of Unity. May be used for testing
+    public static readonly Vector3Int UNITY_ROTATION_ORDER = new Vector3Int(1,2,0);
+
+    // Returns the product of 2 given quaternions
+    public static Vector4 Multiply(Vector4 q1, Vector4 q2)
+    {
+        return new Vector4(
+            q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y,
+            q1.w*q2.y + q1.y*q2.w + q1.z*q2.x - q1.x*q2.z,
+            q1.w*q2.z + q1.z*q2.w + q1.x*q2.y - q1.y*q2.x,
+            q1.w*q2.w - q1.x*q2.x - q1.y*q2.y - q1.z*q2.z
+        );
+    }
+
+    // Returns the conjugate of the given quaternion q
+    public static Vector4 Conjugate(Vector4 q)
+    {
+        return Vector4.zero;
+    }
+
+    // Returns the Hamilton product of given quaternions q and v
+    public static Vector4 HamiltonProduct(Vector4 q, Vector4 v)
+    {
+        return Vector4.zero;
+    }
+
+    // Returns a quaternion representing a rotation of theta degrees around the given axis
+    public static Vector4 AxisAngle(Vector3 axis, float theta)
+    {
+        return Vector4.zero;
+    }
+
+    // Returns a quaternion representing the given Euler angles applied in the given rotation order
+    public static Vector4 FromEuler(Vector3 euler, Vector3Int rotationOrder)
+    {
+        return Vector4.zero;
+    }
+
+    // Returns a spherically interpolated quaternion between q1 and q2 at time t in [0,1]
+    public static Vector4 Slerp(Vector4 q1, Vector4 q2, float t)
+    {
+        return Vector4.zero;
+    }
+}
